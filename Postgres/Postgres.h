12129fcf71da2407783ab126dc2bc8c14983555c
John Wang <john@johntwang.com>
2011-03-25 21:07:50 -1000

    Initial Commit

---
+++ Postgres/Postgres.h
@@ -0,0 +1,19 @@
+//
+//  Postgres.h
+//  Postgres
+//
+//  Created by John Wang on 3/25/11.
+//  Copyright 2011 Fresh Blocks LLC. All rights reserved.
+//
+
+#import <PreferencePanes/PreferencePanes.h>
+
+
+@interface Postgres : NSPreferencePane {
+@private
+    
+}
+
+- (void)mainViewDidLoad;
+
+@end
---
+++ Postgres/Postgres.m
@@ -0,0 +1,18 @@
+//
+//  Postgres.m
+//  Postgres
+//
+//  Created by John Wang on 3/25/11.
+//  Copyright 2011 Fresh Blocks LLC. All rights reserved.
+//
+
+#import "Postgres.h"
+
+
+@implementation Postgres
+
+- (void)mainViewDidLoad
+{
+}
+
+@end
